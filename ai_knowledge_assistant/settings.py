49aea15e33c463eedfff172e743b764353abcde7
dharneesh20 <dharneeshvh@gmail.com>
2026-05-22 02:12:25 +0530

    Updated settings for deployment

--- ai_knowledge_assistant/settings.py
+++ ai_knowledge_assistant/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--0!-83nl0#k$ho7bsa!15%^#$he%b4(talh*71p5)lf@il35ky'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
